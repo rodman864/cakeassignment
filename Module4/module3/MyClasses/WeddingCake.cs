6d986de080e1893f50b8eae64f795be376f54d9c
rodman864 <59933094+rodman864@users.noreply.github.com>
2021-01-25 13:45:59 -0500

    Add files via upload

---
+++ Module4/module3/MyClasses/WeddingCake.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace module3.MyClasses
+{
+    public class Cakes
+
+    {
+        public string Flavor { get; set; }
+        public string Type { get; set; }
+
+        public void AddFlavor<F>(F parameter)
+
+        {
+
+            this.Flavor = parameter as string;
+
+        }
+
+    }
+}
+        public class MyClasses
+        {
+
+
+
+        }
+        public class Vanilla
+        {
+            public string MyProperty { get; set; }
+        }
+
+
+
+
+
+
---
+++ Module4/module3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using module3.MyClasses;
+using module3.Module;
+
+namespace module3
+{
+    public class Cakes
+    {
+        public string Flavor { get; set; }
+        public string Type { get; set; }
+
+        public void AddFlavor<T>(T parameter)
+
+        {
+
+            this.Flavor = parameter as string;
+            this.Type = parameter as string;
+
+        }
+        static void Main(string[] args)
+        {
+            Cakes myCakes = new Cakes();
+            myCakes.Type = "Wedding";
+            myCakes.AddFlavor<string>("Vanilla");
+
+            Cakes myCakes2 = new Cakes();
+            myCakes.Type = "Birthday";
+             myCakes.AddFlavor<string>("Chocolate");
+
+
+
+
+        }
+    }
+    
+}
+
